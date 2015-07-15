c81d82a8db371622f46c9ad4eb197847cefdee7f
Evan Waters <evan.waters@gmail.com>
2015-07-15 12:31:52 -0700

    Can get name, email and phone numbers from address book

--- SnailMail/AddressBookRequestViewController.swift
+++ SnailMail/AddressBookRequestViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import AddressBook
 
 class AddressBookRequestViewController: UIViewController {
-
-    let addressBookRef: ABAddressBook = ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
+    
+    var addressBook:ABAddressBook!
+    var addressBookContacts = [ABRecord]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +26,19 @@ class AddressBookRequestViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func getContacts() {
+        addressBookContacts = ABAddressBookCopyArrayOfAllPeople(addressBook).takeRetainedValue() as NSArray as [ABRecord]
+    }
+    
     func checkAuthorizationStatus() {
         let authorizationStatus = ABAddressBookGetAuthorizationStatus()
         switch authorizationStatus {
         case .Denied, .Restricted:
             println("Unauthorized")
-            performSegueWithIdentifier("initialWelcomeScreen", sender: nil)
         case .Authorized:
+            addressBook =  ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
+            getContacts()
             println("Authorized")
-            performSegueWithIdentifier("initialWelcomeScreen", sender: nil)
         case .NotDetermined:
             promptForAddressBookRequestAccess()
         }
@@ -41,13 +46,14 @@ class AddressBookRequestViewController: UIViewController {
     }
     
     func promptForAddressBookRequestAccess() {
+        
         var err: Unmanaged<CFError>? = nil
         
-        ABAddressBookRequestAccessWithCompletion(addressBookRef) {
+        ABAddressBookRequestAccessWithCompletion(addressBook) {
             (granted: Bool, error: CFError!) in
             dispatch_async(dispatch_get_main_queue()) {
                 if !granted {
-                    self.performSegueWithIdentifier("initialWelcomeScreen", sender: nil)
+                    self.displayUnauthorizedAddessBookAlert()
                 } else {
                     self.accessGranted()
                 }
@@ -57,10 +63,67 @@ class AddressBookRequestViewController: UIViewController {
     
     func accessGranted() {
         println("Access granted!")
-        performSegueWithIdentifier("initialWelcomeScreen", sender: nil)
+        addressBook =  ABAddressBookCreateWithOptions(nil, nil).takeRetainedValue()
+        getContacts()
+    }
+    
+    func openSettings() {
+        let url = NSURL(string: UIApplicationOpenSettingsURLString)
+        UIApplication.sharedApplication().openURL(url!)
+    }
+    
+    func displayUnauthorizedAddessBookAlert() {
+        let cantAccessAddressBookAlert = UIAlertController(title: "No Address Book Access",
+            message: "You have not enabled address book access.",
+            preferredStyle: .Alert)
+        cantAccessAddressBookAlert.addAction(UIAlertAction(title: "Change Settings",
+            style: .Default,
+            handler: { action in
+                self.openSettings()
+        }))
+        cantAccessAddressBookAlert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+        presentViewController(cantAccessAddressBookAlert, animated: true, completion: nil)
     }
     
     
+    @IBAction func nextButtonTapped(sender: AnyObject) {
+        performSegueWithIdentifier("initialWelcomeScreen", sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "initialWelcomeScreen" {
+            if addressBookContacts.count > 0 {
+                for person in addressBookContacts {
+                    println(ABRecordCopyCompositeName(person).takeRetainedValue())
+                    
+                    let firstName:String = ABRecordCopyValue(person, kABPersonFirstNameProperty).takeRetainedValue() as! String
+                    let lastName:String = ABRecordCopyValue(person, kABPersonLastNameProperty).takeRetainedValue() as! String
+                    
+                    println(firstName)
+                    println(lastName)
+                    
+                    var phones : ABMultiValueRef = ABRecordCopyValue(person, kABPersonPhoneProperty).takeUnretainedValue() as ABMultiValueRef
+                        
+                    for(var numberIndex : CFIndex = 0; numberIndex < ABMultiValueGetCount(phones); numberIndex++) {
+                        let phoneUnmaganed = ABMultiValueCopyValueAtIndex(phones, numberIndex)
+                        
+                        let phoneNumber : NSString = phoneUnmaganed.takeUnretainedValue() as! NSString
+                        println(phoneNumber)
+                    }
+                    
+                    var emails : ABMultiValueRef = ABRecordCopyValue(person, kABPersonEmailProperty).takeUnretainedValue() as ABMultiValueRef
+                    
+                    for(var numberIndex : CFIndex = 0; numberIndex < ABMultiValueGetCount(emails); numberIndex++) {
+                        let emailUnmaganed = ABMultiValueCopyValueAtIndex(emails, numberIndex)
+                        
+                        let email : NSString = emailUnmaganed.takeUnretainedValue() as! NSString
+                        println(email)
+                    }
+                    
+                }
+            }
+        }
+    }
     
 
     /*
