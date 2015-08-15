39df8aa6026d354096a197ec9eb60c92e82c9b18
Evan Waters <evan.waters@gmail.com>
2015-08-14 17:26:26 -0700

    Added Flurry for analytics

---
+++ Slowpost-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Slowpost-Bridging-Header.h
+//  Slowpost
+//
+//  Created by Evan Waters on 8/14/15.
+//  Copyright (c) 2015 Evan Waters. All rights reserved.
+//
+
+#ifndef Slowpost_Slowpost_Bridging_Header_h
+#define Slowpost_Slowpost_Bridging_Header_h
+
+#import "Flurry.h"
+
+#endif
