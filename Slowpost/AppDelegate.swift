78f3297f295c78902c9cc98b18eb4b23841945cb
Evan Waters <evan.waters@gmail.com>
2015-08-16 10:04:12 -0700

    Revert "Rebuilt cocopods again; updated App icon with yellow accent."
    This reverts commit 26228940ec4b85d57df5b539c403349dd14f1ac9.
    
    Moving back and back

--- Slowpost/AppDelegate.swift
+++ Slowpost/AppDelegate.swift
@@ -25,7 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Do some additional configuration if needed here
         BITHockeyManager.sharedHockeyManager().startManager()
         BITHockeyManager.sharedHockeyManager().authenticator.authenticateInstallation()
-        BITHockeyManager.sharedHockeyManager().testIdentifier()
         
         application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound | UIUserNotificationType.Alert | UIUserNotificationType.Badge, categories: nil))
         
