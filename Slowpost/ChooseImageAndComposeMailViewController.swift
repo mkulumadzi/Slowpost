11cfd41034344d1334127543e5ff84052cd221c3
Evan Waters <evan.waters@gmail.com>
2015-12-10 17:38:40 -0800

    Compose window pops up while editing, with Done button

--- Slowpost/ChooseImageAndComposeMailViewController.swift
+++ Slowpost/ChooseImageAndComposeMailViewController.swift
@@ -19,6 +19,13 @@ class ChooseImageAndComposeMailViewController: UIViewController, UINavigationCon
     var cardPhotos = [UIImage]()
     var userPhotos = [UIImage]()
     var fetchResult:PHFetchResult!
+    var clearPhotoButton:UIButton!
+    var textEntered:String!
+    var composeTopBorder = UIView()
+    var composeTopBorderDefaultTop = NSLayoutConstraint()
+    var composeTopBorderKeyboardTop = NSLayoutConstraint()
+    var composeTextView = UITextView()
+    var doneEditingButton:UIButton!
     
     @IBOutlet weak var toLabel: UILabel!
     @IBOutlet weak var photoCollection: UICollectionView!
@@ -37,7 +44,61 @@ class ChooseImageAndComposeMailViewController: UIViewController, UINavigationCon
         getCards()
         getImagesFromCameraRoll()
         
+        initializeClearPhotoButton()
+        initializeDoneEditingButton()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidShow:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardHide:", name: UIKeyboardWillHideNotification, object: nil)
+        resignFirstResponder()
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+
+    }
+    
+    // Initializing buttons
+    
+    func initializeClearPhotoButton() {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0)
+        clearPhotoButton = UIButton(frame: rect)
+        clearPhotoButton.setImage(UIImage(named: "remove")!.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        clearPhotoButton.tintColor = UIColor.whiteColor()
+        clearPhotoButton.addTarget(self, action: "clearPhoto", forControlEvents: .TouchUpInside)
+        
+    }
+    
+    func clearPhoto() {
+        print("Photo being cleared!")
+        for subview in view.subviews {
+            if let composeView = subview as? ComposeView {
+                for childview in composeView.subviews {
+                    if let composeTextView = childview as? UITextView {
+                        textEntered = composeTextView.text
+                    }
+                }
+                composeView.removeFromSuperview()
+            }
+        }
+    }
+    
+    func initializeDoneEditingButton() {
+        doneEditingButton = UIButton()
+        view.addSubview(doneEditingButton)
+        doneEditingButton.backgroundColor = slowpostDarkGreen
+        doneEditingButton.setTitle("Done", forState: .Normal)
+        doneEditingButton.titleLabel!.font = UIFont(name: "OpenSans-Semibold", size: 15.0)
+        doneEditingButton.titleLabel!.textColor = UIColor.whiteColor()
+        doneEditingButton.addTarget(self, action: "doneEditing", forControlEvents: .TouchUpInside)
+        doneEditingButton.hidden = true
+    }
+    
+    func doneEditing() {
+        view.endEditing(true)
+    }
+    
+    //
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -245,7 +306,7 @@ class ChooseImageAndComposeMailViewController: UIViewController, UINavigationCon
     // Set up compose view
     
     func addComposeView() {
-        let composeView = UIView()
+        let composeView = ComposeView()
         composeView.backgroundColor = UIColor.lightGrayColor()
         view.addSubview(composeView)
         
@@ -306,10 +367,31 @@ class ChooseImageAndComposeMailViewController: UIViewController, UINavigationCon
     
         NSLayoutConstraint.activateConstraints([topClearBackground, leadingClearBackground, clearBackgroundWidth, clearBackgroundHeight])
         
-        let composeTextView = UITextView()
+        clearButtonBackground.addSubview(clearPhotoButton)
+        
+        composeTopBorder.backgroundColor = UIColor.lightGrayColor()
+        composeView.addSubview(composeTopBorder)
+        
+        composeTopBorderDefaultTop = NSLayoutConstraint(item: composeTopBorder, attribute: .Top, relatedBy: .Equal, toItem: imageContainerView, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        composeTopBorderKeyboardTop = NSLayoutConstraint(item: composeTopBorder, attribute: .Top, relatedBy: .Equal, toItem: toLabel, attribute: .Bottom, multiplier: 1.0, constant: 10.0)
+        let leadingBorder = NSLayoutConstraint(item: composeTopBorder, attribute: .Leading, relatedBy: .Equal, toItem: composeView, attribute: .Leading, multiplier: 1.0, constant: 0.0)
+        let trailingBorder = NSLayoutConstraint(item: composeTopBorder, attribute: .Trailing, relatedBy: .Equal, toItem: composeView, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
+        let borderHeight = NSLayoutConstraint(item: composeTopBorder, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 3.0)
+        
+        composeTopBorderDefaultTop.priority = 999
+        composeTopBorderKeyboardTop.priority = 251
+        
+        composeTopBorder.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints([composeTopBorderDefaultTop, composeTopBorderKeyboardTop, leadingBorder, trailingBorder, borderHeight])
+        
+        if textEntered != nil {
+            composeTextView.text = textEntered
+        }
+        composeTextView.font = UIFont(name: "OpenSans-Light", size: 17.0)
+        composeTextView.textColor = slowpostBlack
         composeView.addSubview(composeTextView)
         
-        let topCompose = NSLayoutConstraint(item: composeTextView, attribute: .Top, relatedBy: .Equal, toItem: imageContainerView, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        let topCompose = NSLayoutConstraint(item: composeTextView, attribute: .Top, relatedBy: .Equal, toItem: composeTopBorder, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
         let leadingCompose = NSLayoutConstraint(item: composeTextView, attribute: .Leading, relatedBy: .Equal, toItem: composeView, attribute: .Leading, multiplier: 1.0, constant: 0.0)
         let trailingCompose = NSLayoutConstraint(item: composeTextView, attribute: .Trailing, relatedBy: .Equal, toItem: composeView, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
         let bottomCompose = NSLayoutConstraint(item: composeTextView, attribute: .Bottom, relatedBy: .Equal, toItem: composeView, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
@@ -318,10 +400,41 @@ class ChooseImageAndComposeMailViewController: UIViewController, UINavigationCon
         
         NSLayoutConstraint.activateConstraints([topCompose, leadingCompose, trailingCompose, bottomCompose])
         
-
+        
         
     }
-
-
+    
+    // Modifying view when keyboard is shown
+    
+    func keyboardShow(notification: NSNotification) {
+        composeTopBorderDefaultTop.priority = 251
+        composeTopBorderKeyboardTop.priority = 999
+        updateViewConstraints()
+        
+        let userInfo = notification.userInfo!
+        var r = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        r = composeTextView.convertRect(r, fromView:nil)
+        composeTextView.contentInset.bottom = r.size.height + 60.0
+        composeTextView.scrollIndicatorInsets.bottom = r.size.height + 60.0
+    }
+    
+    func keyboardHide(notification:NSNotification) {
+        composeTopBorderDefaultTop.priority = 999
+        composeTopBorderKeyboardTop.priority = 251
+        updateViewConstraints()
+        doneEditingButton.hidden = true
+    }
+    
+    func keyboardDidShow(notification:NSNotification) {
+        view.bringSubviewToFront(doneEditingButton)
+        
+        let userInfo = notification.userInfo!
+        var r = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        r = composeTextView.convertRect(r, fromView:nil)
+        let y = view.frame.height - r.size.height - 40.0
+        
+        doneEditingButton.frame = CGRect(x: 0, y: y, width: view.frame.width, height: 40.0)
+        doneEditingButton.hidden = false
+    }
 
 }
---
+++ Slowpost/ComposeView.swift
@@ -0,0 +1,21 @@
+//
+//  ComposeView.swift
+//  Slowpost
+//
+//  Created by Evan Waters on 12/10/15.
+//  Copyright © 2015 Evan Waters. All rights reserved.
+//
+
+import UIKit
+
+class ComposeView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
